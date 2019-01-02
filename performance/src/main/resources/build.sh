d42da10ab2e28a69ccd4ce3e3e6dda77a675b94b
landerlyoung <landerlyoung@gmail.com>
2019-01-02 17:15:49 +0800

    add performance test

---
+++ performance/src/main/resources/build.sh
@@ -0,0 +1,32 @@
+
+SRC_DIR=../java
+CODE_DIR=io/landerlyoung/github/kproto/parser/serialtest
+
+mkdir -p $SRC_DIR/$CODE_DIR
+
+rm -r java javalite wire
+mkdir java javalite wire
+
+# sed -i "" -e s/serialtest.wire/serialtest.java/g proto.proto
+# protoc --java_out=java proto.proto
+# rm -r $SRC_DIR/$CODE_DIR/java
+# mv -f java/$CODE_DIR/java $SRC_DIR/$CODE_DIR/java
+
+
+# javalite download from https://repo1.maven.org/maven2/com/google/protobuf/protoc-gen-javalite/3.0.0/
+# https://github.com/protocolbuffers/protobuf/blob/master/java/lite.md
+sed -i "" -e s/serialtest.wire/serialtest.javalite/g proto.proto
+protoc --plugin=$HOME/Downloads/protoc-gen-javalite --javalite_out=javalite proto.proto
+rm -r $SRC_DIR/$CODE_DIR/javalite
+mv -f javalite/$CODE_DIR/javalite/ $SRC_DIR/$CODE_DIR/
+
+# wire
+# wire download from https://search.maven.org/remote_content?g=com.squareup.wire&a=wire-compiler&c=jar-with-dependencies&v=LATEST
+# https://search.maven.org/remote_content?g=com.squareup.wire&a=wire-compiler&c=jar-with-dependencies&v=2.2.0
+# https://github.com/square/wire
+sed -i "" -e s/serialtest.javalite/serialtest.wire/g proto.proto
+java -jar ~/Downloads/wire-compiler-2.2.0-jar-with-dependencies.jar  --java_out=wire --proto_path=./ proto.proto
+rm -r $SRC_DIR/$CODE_DIR/wire
+mv -f wire/$CODE_DIR/wire $SRC_DIR/$CODE_DIR/
+
+rm -r java javalite wire
---
+++ performance/src/main/test/kotlin/io/landerlyoung/github/kproto/parser/PerformanceTest.kt
@@ -0,0 +1,22 @@
+package io.landerlyoung.github.kproto.parser
+
+import com.squareup.wire.schema.SchemaLoader
+import org.junit.Test
+import java.io.File
+import kotlin.test.assertTrue
+
+/**
+ * <pre>
+ * Author: landerlyoung@gmail.com
+ * Date:   2018-12-31
+ * Time:   21:49
+ * Life with Passion, Code with Creativity.
+ * </pre>
+ */
+
+class PerformanceTest {
+    @Test
+    fun testTest() {
+        assertTrue(true)
+    }
+}
